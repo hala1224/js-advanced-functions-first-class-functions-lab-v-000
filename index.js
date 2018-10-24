1ea7fb657ee63e6853464c951aa66b04dd6343a3
Natalie Halawani <hala1224@gmail.com>
2018-10-24 21:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arr) {
+   newArr=[];
+  newArr=arr.slice(0,2);
+  
+ return newArr;
+};
+const returnLastTwoDrivers = function(arr) {
+   newArr=[];
+   newArr=arr.slice(arr.length-2,arr.length);
+  
+   return newArr;
+};
+
